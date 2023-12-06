a512f594df98b040dd38eb31ad6e3d773594b256
JONATHAN NASCIMENTO <115124116+mamoto22@users.noreply.github.com>
2023-12-06 16:50:42 -0300

    Add files via upload

---
+++ PrimeiraAPIC#/PrimeiraAPIC#/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using PrimeiraAPIC_.Models;
+using System.Collections.Generic;
+
+namespace PrimeiraAPIC_.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
